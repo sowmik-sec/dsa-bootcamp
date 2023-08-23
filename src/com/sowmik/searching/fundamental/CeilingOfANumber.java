96c2b0cfd31d6a260069b594a18ff308562ba680
sowmik-sec <sowmik.sec@gmail.com>
2023-08-23 19:48:44 +0600

    ceiling of a number code

---
+++ src/com/sowmik/searching/fundamental/CeilingOfANumber.java
@@ -0,0 +1,21 @@
+package com.sowmik.searching.fundamental;
+
+public class CeilingOfANumber {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 7, 10, 10, 11, 13, 14, 20, 28,35};
+        int target = 10;
+        int ans = ceiling(nums, target);
+        System.out.println(ans);
+    }
+    static int ceiling(int[] arr, int target) {
+        int s = 0, e = arr.length-1;
+        while (s<=e) {
+            int m = s + (e-s)/2;
+            if(target>=arr[m]) {
+                s = m+1;
+            }
+            else e = m-1;
+        }
+        return arr[s];
+    }
+}
