557c742b3c021f7cac84ff5d85c99062bf17d375
sowmik-sec <sowmik.sec@gmail.com>
2023-08-23 20:00:27 +0600

    floor of a number code

--- src/com/sowmik/searching/fundamental/FloorOfANumber.java
+++ src/com/sowmik/searching/fundamental/FloorOfANumber.java
@@ -1,21 +1,24 @@
 package com.sowmik.searching.fundamental;
 
-public class CeilingOfANumber {
+public class FloorOfANumber {
     public static void main(String[] args) {
         int[] nums = {2, 3, 7, 10, 10, 11, 13, 14, 20, 28,35};
-        int target = 10;
-        int ans = ceiling(nums, target);
+        int target = 1;
+        int ans = floor(nums, target);
         System.out.println(ans);
     }
-    static int ceiling(int[] arr, int target) {
+    static int floor(int[] arr, int target) {
         int s = 0, e = arr.length-1;
+        if(target<arr[0]) {
+            return -1;
+        }
         while (s<=e) {
             int m = s + (e-s)/2;
-            if(target>=arr[m]) {
-                s = m+1;
+            if(target<=arr[m]) {
+                e = m-1;
             }
-            else e = m-1;
+            else s = m+1;
         }
-        return arr[s];
+        return arr[e];
     }
 }
